d440ca11f8da3da504e063555b6e8d02615c8fef
Steve Ebenezer Paul <stevepaul325@gmail.com>
2017-01-02 10:10:12 +0000

    add initials

--- initials.c
+++ initials.c
@@ -1,4 +1,5 @@
 /* Initials (less comfortable)
+* 
 * Implement your program in a file called initials.c in a directory called initials.
 * Your program should prompt a user for their name using get_string to obtain their name as a string.
 * You may assume that the user’s input will contain only letters (uppercase and/or lowercase) plus single spaces between words. You don’t need to worry about names like Joseph Gordon-Levitt, Conan O’Brien, or David J. Malan!
