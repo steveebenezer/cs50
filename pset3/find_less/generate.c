aa5bf68990b7de661434db0c18ced06d8cfb76eb
Steve Ebenezer Paul <stevepaul325@gmail.com>
2017-01-14 18:09:37 +0000

    Add pset3

---
+++ pset3/find_less/generate.c
@@ -0,0 +1,54 @@
+/**
+ * generate.c
+ *
+ * Generates pseudorandom numbers in [0,MAX), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Validate number of arguments to 2 or 3
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // Convert string argument to integer
+    int n = atoi(argv[1]);
+
+    // Take argument 2 as reference if 2 arguments 
+    // are given as inputs else take time and convert 
+    // it to long int as default argument
+    if (argc == 3)
+    {
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
+    }
+
+    // Print n number of random numbers, where n = atoi(argv[1])
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
